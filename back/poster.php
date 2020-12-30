19be90c1cdc08b6159aaaab7d5b73a4bbd7962f4
ccj1994 <a0changj2@gmail.com>
2020-12-30 13:33:12 +0800

    後臺可以顯示預告片清單

--- back/poster.php
+++ back/poster.php
@@ -6,8 +6,37 @@
     <div style="width:25%;margin:0 1px;background:#999;">預告片排序</div>
     <div style="width:25%;margin:0 1px;background:#999;">操作</div>
   </div>
-  <div style="width:100%;height:200px;overflow:auto;">
+  <div style="width:100%;height:200px;overflow-y:scroll;color:#fff;">
+    <?php
+  $posters=$Poster->all();
+  foreach($posters as $poster){
+    ?>
+    <div class="ct" style="width:100%;display:flex;align-items:center;background:white;margin:1px 0;">
+      <div style="width:25%;margin:0 1px;">
+        <img src="img/<?=$poster['img']?>" style="width:80px;">
+      </div>
+      <div style="width:25%;margin:0 1px;">
+        <input type="text" name="name" value="<?=$poster['name']?>">
+      </div>
+      <div style="width:25%;margin:0 1px;">
+        <input type="button" value="往上">
+        <input type="button" value="往下">
+      </div>
+      <div style="width:25%;margin:0 1px;">
+        <input type="checkbox" name="sh[]" value="<?=$poster['id']?>" <?=($poster['sh']==1)?'checked':'';?>>顯示
+        <input type="checkbox" name="del[]" value="<?=$poster['id']?>">刪除
+        <select name="ani[]" id="">
+          <option value="1" <?=($poster['ani']==1)?'selected':'';?>>淡入淡出</option>
+          <option value="2" <?=($poster['ani']==2)?'selected':'';?>>滑入滑出</option>
+          <option value="3" <?=($poster['ani']==3)?'selected':'';?>>縮放</option>
+        </select>
+      </div>
+    </div>
 
+    <?php
+  }
+  
+  ?>
   </div>
   <div class="ct">
     <input type="submit" value="編輯確定">
@@ -16,19 +45,19 @@
   <hr>
   <h3 class="ct" style="margin:0">新增預告片海報</h3>
   <form action="api/add_poster.php" method="post" enctype="multipart/form-data">
-  
-  
-  <table>
-    <tr>
-      <td>預告片海報</td>
-      <td><input type="file" name="poster" id=""></td>
-      <td>預告片片名:</td>
-      <td><input type="text" name="name" id=""></td>
-    </tr>
-  </table>
-  <div class="ct">
-    <input type="submit" value="新增">
-    <input type="reset" value="重置">
-  </div>
+
+
+    <table>
+      <tr>
+        <td>預告片海報</td>
+        <td><input type="file" name="poster" id=""></td>
+        <td>預告片片名:</td>
+        <td><input type="text" name="name" id=""></td>
+      </tr>
+    </table>
+    <div class="ct">
+      <input type="submit" value="新增">
+      <input type="reset" value="重置">
+    </div>
   </form>
 </div>
