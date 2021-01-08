5c88d0e205f6aecab936a634d7f1608c30d00866
ccj1994 <a0changj2@gmail.com>
2021-01-08 11:27:14 +0800

    完成預告片海報轉場特效

--- front/main.php
+++ front/main.php
@@ -1,18 +1,84 @@
   <style>
-    
+    .ct a{
+      text-decoration:none;
+    }
+    .posters{
+      width:200px;
+      height:260px;
+      margin:auto;
+      text-align:center;
+      position:relative;
+
+    }
+    .posters > div{
+      position:absolute;
+    }
+    .posters img{
+      width:100%;
+    }
+
   </style>
   
   <div class="half" style="vertical-align:top;">
     <h1>預告片介紹</h1>
     <div class="rb tab" style="width:95%;">
-      <div id="abgne-block-20111227">
-        <ul class="lists">
-        </ul>
-        <ul class="controls">
-        </ul>
+      <div class="posters">
+        <?php
+          $posters=$Poster->all(['sh'=>1]," order by rank");
+          foreach( $posters as $key => $poster){
+            echo "<div class='po' id='p{$key}' data-ani='{$poster['ani']}'>";
+            echo "<img src='img/{$poster['img']}'>";
+            echo "<span>{$poster['name']}</span>";
+            echo "</div>";
+          }
+        ?>
       </div>
+      <div class="buttons"></div>
     </div>
   </div>
+  <script>
+  $(".po").hide();
+  $("#p0").show();
+  let pos=$(".po").length;
+  let t=setInterval(ani, 2000);
+  function ani(){
+    //取得目前正在顯示中的海報
+    let now=$(".po:visible");
+    //取得目前正在顯示中的海報的轉場效果
+    let ani=$(now).data('ani');
+    //取得目前正在顯示中的海報的下一張海報
+    let next=$(now).next();
+    //判斷目前正在顯示中的海報是否有下一張海報
+    if($(now).next().length){
+      next=$(now).next();
+    }else{
+      //如果沒有下一張海報,則取得第一張海報
+      next=$("#p0");
+    }
+    switch(ani){
+      case 1:
+        //淡入淡出
+        $(now).fadeOut(1000);
+        $(next).fadeIn(1000);
+        break;
+      case 2:
+        //滑入滑出
+        $(now).slideUp(1000,function(){
+
+          $(next).slideDown(1000);
+        });
+        break;
+      case 3:
+        //縮放
+        $(now).hide(1000);
+        $(next).show(1000);
+        break;
+    }
+    
+  }
+  
+  </script>
+
   <div class="half">
     <h1>院線片清單</h1>
     <div class="rb tab" style="width:95%;display:flex;flex-wrap:wrap;">
